4d48f70885557de4bbd5927482437aa8f6a3b803
dgiggles <dmuhia993@gmail.com>
2023-02-20 13:05:47 -0800

    Print aAzZ

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - code that prints the alphs in lowercase, and then in uppercase,
- * only use putchar
+ * main - program that prints the alphabet in lowercase, and then in uppercase,
+ * You can only use the putchar
  * Return: 0
  */
 
 int main(void)
 {
-	int l = 'a';
-	int up = 'A';
-	while (l <= 'z')'\n';
-	{
-		putchar(1);
-		l += 1;
-	}
-	while (up <= 'Z');
-	{
-		putchar(up);
-		up += 1;
-	}
-		putchar('\n');
-	return (0);
+int lw = 'a';
+int up = 'A';
+while (lw <= 'z')
+{
+putchar(lw);
+lw += 1;
+}
+while (up <= 'Z')
+{
+putchar(up);
+up += 1;
+}
+putchar('\n');
+return (0);
 }
